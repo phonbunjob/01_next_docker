9ebda25b86842f450c8e929fa707de6ed781a808
phonbunjob <pisitbunjob123@gmail.com>
2025-09-15 11:20:14 +0700

    update

--- app/components/Attractions.js
+++ app/components/Attractions.js
@@ -47,7 +47,7 @@ fetchAttractions();
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold text-center mb-10 text-gray-800">
-        Popular Tourist Attractions
+       Phisit Pholphuksa 6600570
       </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
